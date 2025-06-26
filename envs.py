1a0677d9ef9927c4b826ceb5fd67bb8d81dd3876
dhanishahammed <137599211+dhanishahammed@users.noreply.github.com>
2025-06-26 16:21:28 +0530

    envs.py

--- envs.py
+++ envs.py
@@ -1,16 +1,13 @@
 import gym
-import os
 import numpy as np
 import torch
 from gym.spaces.box import Box
-from gym.spaces.discrete import Discrete
 
 from wrapper.benchmarks import *
 from wrapper.monitor import *
 from wrapper.vec_env import VecEnvWrapper
 from wrapper.shmem_vec_env import ShmemVecEnv
 from wrapper.dummy_vec_env import DummyVecEnv
-from packing_env import PackingEnv
 
 try:
     import dm_control2gym
@@ -33,30 +30,25 @@ def make_env(env_id, seed, rank, log_dir, allow_early_resets, args):
             _, domain, task = env_id.split('.')
             env = dm_control2gym.make(domain_name=domain, task_name=task)
         else:
-            env = PackingEnv(
-                setting=args.setting,
-                container_size=args.container_size,
-                item_set=args.item_size_set,
-                data_name=args.dataset_path,
-                load_test_data=args.load_dataset,
-                internal_node_holder=args.internal_node_holder,
-                leaf_node_holder=args.leaf_node_holder,
-                LNES=args.lnes,
-                shuffle=args.shuffle,
-                sample_from_distribution=args.sample_from_distribution,
-                sample_left_bound=args.sample_left_bound,
-                sample_right_bound=args.sample_right_bound
-            )
+            env = gym.make(
+                           env_id,
+                           setting = args.setting,
+                           container_size = args.container_size,
+                           item_set = args.item_size_set,
+                           data_name = args.dataset_path,
+                           load_test_data = args.load_dataset,
+                           internal_node_holder = args.internal_node_holder,
+                           leaf_node_holder = args.leaf_node_holder,
+                           LNES = args.lnes,
+                           shuffle = args.shuffle,
+                           sample_from_distribution = args.sample_from_distribution,
+                           sample_left_bound = args.sample_left_bound,
+                           sample_right_bound = args.sample_right_bound
+                           )
 
         env.seed(seed + rank)
 
-        if not hasattr(env, 'action_space'):
-            env.action_space = Discrete(args.leaf_node_holder)
-
-        if not hasattr(env, 'observation_space'):
-            graph_size = args.internal_node_holder + args.leaf_node_holder + 1
-            feature_dim = max(args.internal_node_length, 8, 6)
-            env.observation_space = Box(low=0.0, high=1.0, shape=(graph_size, feature_dim), dtype=np.float32)
+        obs_shape = env.observation_space.shape
 
         if str(env.__class__.__name__).find('TimeLimit') >= 0:
             env = TimeLimitMask(env)
@@ -68,8 +60,11 @@ def make_env(env_id, seed, rank, log_dir, allow_early_resets, args):
                 allow_early_resets=allow_early_resets)
 
         if len(env.observation_space.shape) == 3:
-            raise NotImplementedError("CNN models work only for atari,\nplease use a custom wrapper for a custom pixel input env.\n")
+            raise NotImplementedError(
+                "CNN models work only for atari,\n"
+                "please use a custom wrapper for a custom pixel input env.\n")
 
+        # If the input has shape (W,H,3), wrap for PyTorch convolutions
         obs_shape = env.observation_space.shape
         if len(obs_shape) == 3 and obs_shape[2] in [1, 3]:
             env = TransposeImage(env, op=[2, 0, 1])
@@ -77,43 +72,47 @@ def make_env(env_id, seed, rank, log_dir, allow_early_resets, args):
         return env
     return _thunk
 
-def make_vec_envs(args, log_dir, allow_early_resets):
+def make_vec_envs(args,
+                  log_dir,
+                  allow_early_resets):
+
     env_name = args.id
     seed = args.seed
     num_processes = args.num_processes
     device = args.device
 
-    envs = [make_env(env_name, seed, i, log_dir, allow_early_resets, args) for i in range(num_processes)]
+    envs = [
+        make_env(env_name, seed, i, log_dir, allow_early_resets, args)
+        for i in range(num_processes)
+    ]
 
     if len(envs) >= 1:
-        dummy_env = PackingEnv(
-            setting=args.setting,
-            container_size=args.container_size,
-            item_set=args.item_size_set,
-            data_name=args.dataset_path,
-            load_test_data=args.load_dataset,
-            internal_node_holder=args.internal_node_holder,
-            leaf_node_holder=args.leaf_node_holder,
-            LNES=args.lnes,
-            shuffle=args.shuffle,
-            sample_from_distribution=args.sample_from_distribution,
-            sample_left_bound=args.sample_left_bound,
-            sample_right_bound=args.sample_right_bound
-        )
+        """
+            If you don't specify observation_space, we'll have to create a dummy
+            environment to get it.
+        """
+        env = gym.make(env_name,
+                       setting = args.setting,
+                       item_set = args.item_size_set,
+                       container_size=args.container_size,
+                       internal_node_holder = args.internal_node_holder,
+                       leaf_node_holder = args.leaf_node_holder,
+                       LNES = args.lnes,
+                       shuffle=args.shuffle,
+                       sample_from_distribution=args.sample_from_distribution,
+                       sample_left_bound=args.sample_left_bound,
+                       sample_right_bound=args.sample_right_bound
+                       )
 
-        if not hasattr(dummy_env, 'action_space'):
-            dummy_env.action_space = Discrete(args.leaf_node_holder)
-        if not hasattr(dummy_env, 'observation_space'):
-            graph_size = args.internal_node_holder + args.leaf_node_holder + 1
-            feature_dim = max(args.internal_node_length, 8, 6)
-            dummy_env.observation_space = Box(low=0.0, high=1.0, shape=(graph_size, feature_dim), dtype=np.float32)
-
-        spaces = [dummy_env.observation_space, dummy_env.action_space]
+        spaces = [env.observation_space, env.action_space]
         envs = ShmemVecEnv(envs, spaces, context='fork')
+
     else:
         envs = DummyVecEnv(envs)
 
     envs = VecPyTorch(envs, device)
+
+
     return envs
 
 class TimeLimitMask(gym.Wrapper):
@@ -121,25 +120,37 @@ class TimeLimitMask(gym.Wrapper):
         obs, rew, done, info = self.env.step(action)
         if done and self.env._max_episode_steps == self.env._elapsed_steps:
             info['bad_transition'] = True
+
         return obs, rew, done, info
 
     def reset(self, **kwargs):
         return self.env.reset(**kwargs)
 
+
+
 class TransposeObs(gym.ObservationWrapper):
     def __init__(self, env=None):
+        """
+        Transpose observation space (base class)
+        """
         super(TransposeObs, self).__init__(env)
+
 
 class TransposeImage(TransposeObs):
     def __init__(self, env=None, op=[2, 0, 1]):
+        """
+        Transpose observation space for images
+        """
         super(TransposeImage, self).__init__(env)
-        assert len(op) == 3, f"Error: Operation {op} must be of length 3"
+        assert len(op) == 3, "Error: Operation, {str(op)}, must be dim3"
         self.op = op
         obs_shape = self.observation_space.shape
         self.observation_space = Box(
             self.observation_space.low[0, 0, 0],
-            self.observation_space.high[0, 0, 0],
-            [obs_shape[op[0]], obs_shape[op[1]], obs_shape[op[2]]],
+            self.observation_space.high[0, 0, 0], [
+                obs_shape[self.op[0]], obs_shape[self.op[1]],
+                obs_shape[self.op[2]]
+            ],
             dtype=self.observation_space.dtype)
 
     def observation(self, ob):
@@ -147,8 +158,10 @@ class TransposeImage(TransposeObs):
 
 class VecPyTorch(VecEnvWrapper):
     def __init__(self, venv, device):
+        """Return only every `skip`-th frame"""
         super(VecPyTorch, self).__init__(venv)
         self.device = device
+        # TODO: Fix data types
 
     def reset(self):
         obs = self.venv.reset()
@@ -157,7 +170,9 @@ class VecPyTorch(VecEnvWrapper):
 
     def step_async(self, actions):
         if isinstance(actions, torch.LongTensor):
+            # Squeeze the dimension for discrete actions
             actions = actions.squeeze(1)
+
         self.venv.step_async(actions)
 
     def step_wait(self):
